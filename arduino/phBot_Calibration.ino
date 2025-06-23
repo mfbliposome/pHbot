3a85d92d87bcd034201f5b3e4227b2b167120518
Donomania <115245251+Donomania@users.noreply.github.com>
2025-06-23 16:36:09 -0400

    Update phBot_Calibration.ino
    Updated and fixed to work with Arduino serial monitor. Correctly measures the input of two known pH values and calculates the slope and y-intercept for them. Currently self-contained (not currently interfacing with host code.

--- arduino/phBot_Calibration.ino
+++ arduino/phBot_Calibration.ino
@@ -1,74 +1,81 @@
 int pHProbe = A7; //change # to corresponding analog pin if needed
 int pHVolts1 = 0;
 int pHVolts2 = 0;
-float pHValue1 = 0.00;
-float pHValue2 = 0.00;
+int pHValue1 = 0.00;
+int pHValue2 = 0.00;
 float pHSlope = 0.0000;
 float pHIntercept = 0.0000;
 
 void setup() {
   Serial.begin(9600);
   pinMode(pHProbe, INPUT);
-
 }
 
 void loop() {
-  pHValue1 = whatValuepH(1);
-  pHVolts1 = readVolts();
-  pHValue2 = whatValuepH(2);
-  pHVolts2 = readVolts();
-  pHSlope = calculateSlope(pHValue2, pHValue1, pHVolts2, pHVolts1);
-  pHIntercept = calculateIntercept(pHValue2, pHValue1, pHVolts2, pHVolts1, pHSlope);
-  while(2 > 1) {
-    Serial.println("The millivolt to pH slope is " + String(pHSlope) + " and the y-intercept is " + String(pHIntercept));
-    delay(5000);
+  pHValue1 = whatValuepH(1); //Preps the first pH value for reading, requires the user to input the first known pH value.
+  pHVolts1 = readVolts(); //Measures the first pH value as milivolts.
+  Serial.println("The reading of pH " + String(pHValue1) + " in milivolts is: " + String(pHVolts1)); //Placeholder print line to verify first reading.
+  pHValue2 = whatValuepH(2); //Preps the second pH value for reading, requires the user to input the second known pH value.
+  pHVolts2 = readVolts(); //Measures the second pH value as milivolts.
+  Serial.println("The reading of pH " + String(pHValue2) + " in milivolts is: " + String(pHVolts2)); //Placeholder print line to verify second reading.
+  delay(100); //Small delay to not interfere with the print lines, possibly not needed
+  pHSlope = calculateSlope(pHValue2, pHValue1, pHVolts2, pHVolts1); //Calculates the slope using the two data points.
+  Serial.println("The calibration slope is " + String(pHSlope)); //Placeholder print line to verify the slope value, displayed with two decimal points.
+  pHIntercept = calculateIntercept(pHValue2, pHValue1, pHVolts2, pHVolts1, pHSlope); //Calculates the y-intercepts using the two data points and slope.
+  Serial.println("The calibration intercept is " + String(pHIntercept)); //Placeholder print line verify the y-intercept, displayed with two decimal points.
+  Serial.println("The calibration formula is now y = " + String(pHSlope) + "x + " + String(pHIntercept)); //Placeholder print to demonstate the formula, , displayed with two decimal points.
+  while(true) {
+    //Placeholder loop to prevent the program from looping.
   }
-
 }
 
-float whatValuepH (int bufferNumber) {
-  float pHBuffer = 0.00;
-  Serial.println("pH Value of buffer" + String(bufferNumber) + ":");
-  pHBuffer = Serial.read();
-  return pHBuffer;
+int whatValuepH (int bufferNumber) {
+  int pHBuffer = 0; //Local variable used to hold the value of the input pH.
+  Serial.println("Whole  number pH Value of buffer " + String(bufferNumber) + " :");
+  while(pHBuffer == 0) {
+    if(Serial.available() > 0) {
+      pHBuffer = Serial.parseInt(); //Waits for a user to input the pH value, and will not continue until a non-trivial value is entered.
+    }
+  }
+  return pHBuffer; //This will return the value back to the central program for later use.
 }
 
 int readVolts () {
-  int voltBuffer = 0;
-  int stableCounter = 0;
-  int previousVolt = 0;
-  while(stableCounter < 10) {
-    int currentVolt = 0;
-    currentVolt = analogRead(pHProbe);
-    if(currentVolt > (previousVolt - 2) && currentVolt < (previousVolt + 2)) {
-      stableCounter++;
+  int voltBuffer = 0; //Local variable used to hold the value of the stabilized input.
+  int stableCounter = 0; //Local variable used to count the amount of successive readings within tolerance
+  int previousVolt = 0; //Local variable used to store the voltage value of the previous cycle of the while loop for comparison.
+  while(stableCounter < 10) { //Number of consecutive similar comparisons for a reading.
+    int currentVolt = 0; //Internal local variable used to compare with previousVolt.
+    currentVolt = analogRead(pHProbe); //Assign the current value to currentVolt.
+    if(currentVolt > (previousVolt - 2) && currentVolt < (previousVolt + 2)) { //Tolerance of comparison (Number to use = tolerance + 1)
+      stableCounter++; //If the previous and current values are within tolerance, increment the stability counter.
     }
     else {
-      stableCounter = 0;
+      stableCounter = 0; //Otherwise (in other words, outside of tolerance), reset the stability counter.
     }
-    previousVolt = currentVolt;
-    delay(500);
+    previousVolt = currentVolt; //Set the current value to be the previous value for the next cycle.
+    delay(500); //Frequency of measurements
   }
-  voltBuffer = previousVolt; 
-  return voltBuffer;
+  voltBuffer = previousVolt; //Once stable, sets the last stable value as the reading for the buffer.
+  return voltBuffer;//This will return the value back to the central program for later use.
 }
 
-float calculateSlope (float y2, float y1, int x2, int x1) {
-  float slope = 0.0000;
-  float deltay = 0.00;
+float calculateSlope (int y2, int y1, int x2, int x1) {
+  float slope = 0.000;
+  int deltay = 0;
   int deltax = 0;
   deltay = y2 - y1;
-  deltax = x2 - x2;
-  slope = (deltay)/(1000 * deltax);
-  return slope;
+  deltax = x2 - x1;
+  slope = (float)deltay / (float)deltax; //Slope calculations to find the slope.
+  return slope; //This will return the value back to the central program for later use.
 }
 
-float calculateIntercept (float y2, float y1, int x2, int x1, float m) {
-  float b = 0.0000;
-  float b1 = 0.0000;
-  float b2 = 0.0000;
-  b1 = y1 - (m)*(x1)/1000;
-  b2 = y2 - (m)*(x2)/1000;
-  b = (b1 + b2) / 2;
-  return b;
+float calculateIntercept (int y2, int y1, int x2, int x1, float m) {
+  float b = 0.000;
+  float b1 = 0.000;
+  float b2 = 0.000;
+  b1 = y1 - (m * x1); //Intercept calculation for first pH value.
+  b2 = y2 - (m * x2); //Intercept calculation for second pH value.
+  b = (b1 + b2) / 2; //In the event where b1 and b2 are different (for whatever reason), this will use the average of the two.
+  return b; //Intercept calculation for first pH value.
 }
